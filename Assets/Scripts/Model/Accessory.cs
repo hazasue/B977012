3dca9b042d5a830bbf168c3a6c5733b43ca53e5d
ChangHyeon Kim <hazasue@gmail.com>
2023-10-03 19:20:57 +0900

    :tada: Initialize Accessory

---
+++ Assets/Scripts/Model/Accessory.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accessory : MonoBehaviour
+{
+    // attributes
+    private string stat;
+    private float value;
+
+    public void Init() {}
+    
+    private void ReflectOptionToPlayer() {}
+}
