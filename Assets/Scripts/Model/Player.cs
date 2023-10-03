112ef6abd28f86caf807e5dbe9339338f882f061
ChangHyeon Kim <hazasue@gmail.com>
2023-10-03 19:20:35 +0900

    :tada: Initialize Player

---
+++ Assets/Scripts/Model/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    // attributes
+    private float pickupRange;
+    private int critical;
+
+    // associations
+    private Inventory inventory;
+    private PlayerLevel level;
+    private Skill skill;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // methods
+    public override void Init() {}
+    
+    protected override void Attack() {}
+    
+    private void PickUpItems() {}
+
+    public void GainExp(int value) {}
+
+    public void GainCoins(int value) {}
+
+    public Inventory GetInventory() { return Inventory; }
+
+    public PlayerLevel GetLevelInfo() { return playerLevel; }
+
+
+
+}
