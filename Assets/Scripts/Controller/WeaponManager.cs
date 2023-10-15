4c51e2070e5ded0fc420a104d83c5d4b1848f372
ChangHyeon Kim <hazasue@gmail.com>
2023-10-15 14:02:15 +0900

    :tada: Init WeaponManager

---
+++ Assets/Scripts/Controller/WeaponManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    private static string DEFAULT_DATATABLE_WEAPON = "DataTable_Weapon"
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Init()
+    {
+        
+    }
+}
