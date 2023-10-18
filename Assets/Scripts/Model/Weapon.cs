6b8459c2edcb9801662707f0fbc66addf78303ae
ChangHyeon Kim <hazasue@gmail.com>
2023-10-18 23:50:44 +0900

    :hammer: Update Weapon: Init, Instantiate/Active weapon objects

--- Assets/Scripts/Model/Weapon.cs
+++ Assets/Scripts/Model/Weapon.cs
@@ -4,33 +4,111 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    private enum WeaponType
+    public enum WeaponType
     {
-        melee,
-        ranged,
-        tracking,
-        chaining,
-        beam,
-        explosive,
+        MELEE,
+        RANGED,
+        TRACKING,
+        CHAINING,
+        BEAM,
+        EXPLOSIVE,
     }
+
+    private static int DEFAULT_OBJECT_COUNT = 200;
     
     // attributes
-    private float damage;
+    private string code;
+    private string name;
+    private int damage;
     private float duration;
     private float delay;
     private int projectile;
     private float range;
+    private float speed;
     private WeaponType weaponType;
 
+    private bool enableToAttack;
+
+    private Queue<WeaponObject> weaponObjects;
+
+    private Transform instanceTransform;
+
     // methods
-    public void Init() {}
+    public void Init(WeaponInfo weaponInfo)
+    {
+        code = weaponInfo.GetCode();
+        name = weaponInfo.GetName();
+        damage = weaponInfo.GetDamage();
+        duration = weaponInfo.GetDuration();
+        delay = weaponInfo.GetDelay();
+        projectile = weaponInfo.GetProjectile();
+        range = weaponInfo.GetRange();
+        speed = weaponInfo.GetSpeed();
+        weaponType = weaponInfo.GetType();
+
+        enableToAttack = true;
+        
+        weaponObjects = new Queue<WeaponObject>();
+
+        InstantiateWeaponObjects();
+        StartCoroutine(EnableToAttack());
+
+        switch (weaponType)
+        {
+            case WeaponType.MELEE:
+                instanceTransform = this.transform;
+                break;
+            
+            case WeaponType.TRACKING:
+                instanceTransform = GameObject.Find("Projectile Transform").transform;
+                break;
+            
+            default:
+                break;
+        }
+    }
     
     public void UpgradeWeapon() {}
-    
-    private void InstantiateWeaponObjects() {}
 
-    private IEnumerator ActiveWeaponObject()
+    private void InstantiateWeaponObjects()
     {
-        yield break;
+        WeaponObject tempObject;
+        for (int i = 0; i < DEFAULT_OBJECT_COUNT; i++)
+        {
+            tempObject =
+                Instantiate(Resources.Load<WeaponObject>("Prefabs/weapons/" + code + "_object"), instanceTransform, true);
+            weaponObjects.Enqueue(tempObject);
+            tempObject.gameObject.SetActive(false);
+        }
     }
+
+    public void ActiveWeaponObject(Vector3 attackDirection)
+    {
+        if (!enableToAttack) return;
+
+        enableToAttack = false;
+
+        WeaponObject tempObject = weaponObjects.Dequeue();
+        tempObject.gameObject.SetActive(true);
+        tempObject.Init(damage, speed, attackDirection, weaponType);
+        tempObject.transform.position = this.transform.position;
+        weaponObjects.Enqueue(tempObject);
+
+        StartCoroutine(UnActiveWeaponObject(tempObject, duration));
+        StartCoroutine(EnableToAttack());
+    }
+
+    private IEnumerator EnableToAttack()
+    {
+        yield return new WaitForSeconds(delay);
+
+        enableToAttack = true;
+    }
+
+    private IEnumerator UnActiveWeaponObject(WeaponObject weaponObject, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+
+        weaponObject.gameObject.SetActive(false);
+    } 
 }
