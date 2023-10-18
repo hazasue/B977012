c8e6db15a9816f5bd3f05bf22be9fc26f41750ef
ChangHyeon Kim <hazasue@gmail.com>
2023-10-18 23:47:39 +0900

    :hammer: Update WeaponManager: Save weapon infos in script

--- Assets/Scripts/Controller/WeaponManager.cs
+++ Assets/Scripts/Controller/WeaponManager.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
-    private static string DEFAULT_DATATABLE_WEAPON = "DataTable_Weapon"
+    private static WeaponManager instance;
+    
+    private static string CSV_FILENAME_WEAPON = "DataTable_Weapon";
+
+    private Dictionary<string, WeaponInfo> weaponInfos;
     
     // Start is called before the first frame update
     void Awake()
@@ -20,6 +24,42 @@ public class WeaponManager : MonoBehaviour
 
     private void Init()
     {
-        
+        weaponInfos = new Dictionary<string, WeaponInfo>();
+
+        List<Dictionary<string, object>> WeaponDB = CSVReader.Read(CSV_FILENAME_WEAPON);
+        foreach (Dictionary<string, object> weaponInfo in WeaponDB)
+        {
+            weaponInfos.Add(weaponInfo["WeaponCode"].ToString(),
+                new WeaponInfo(weaponInfo["WeaponCode"].ToString(),
+                    weaponInfo["WeaponName"].ToString(),
+                    weaponInfo["WeaponType"].ToString(),
+                    (int)weaponInfo["Damage"],
+                    float.Parse(weaponInfo["Duration"].ToString()),
+                    float.Parse(weaponInfo["Delay"].ToString()),
+                    (int)weaponInfo["Projectile"],
+                    float.Parse(weaponInfo["Range"].ToString()),
+                    float.Parse(weaponInfo["Speed"].ToString())
+                ));
+        }
     }
+    
+    public static WeaponManager GetInstance()
+    {
+        if (instance != null) return instance;
+        instance = FindObjectOfType<WeaponManager>();
+        if (instance == null) Debug.Log("There's no active WeaponManager object");
+        return instance;
+    }
+
+    public WeaponInfo GetWeaponInfo(string code)
+    {
+        if (weaponInfos.ContainsKey(code)) return weaponInfos[code];
+        else
+        {
+            Debug.Log("Invalid weapon code: " + code);
+            return null;
+        }
+    }
+
+    public Dictionary<string, WeaponInfo> GetAllWeaponInfos() { return weaponInfos; }
 }
