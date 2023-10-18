894cbd0577f0a281bc06fcc6110a56e80fa953ed
ChangHyeon Kim <hazasue@gmail.com>
2023-10-18 23:46:02 +0900

    :tada: Init CommonUIManager

---
+++ Assets/Scripts/Controller/CommonUIManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CommonUIManager : MonoBehaviour
+{
+    private static CommonUIManager instance;
+    
+    public void MoveScene(string name)
+    {
+        switch (name)
+        {
+            case "Title":
+            case "CharacterSelect":
+            case "Lobby":
+            case "InGame":
+                SceneManager.LoadScene(name);
+                break;
+            
+            default:
+                Debug.Log("Invalid scene name: " + name);
+                break;
+        }
+    }
+    
+    public static CommonUIManager GetInstance()
+    {
+        if (instance != null) return instance;
+        instance = FindObjectOfType<CommonUIManager>();
+        if (instance == null) Debug.Log("There's no active CommonUIManager object");
+        return instance;
+    }
+}
