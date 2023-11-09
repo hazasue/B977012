41bff82279ce7217c9b07cac266e1d0672c27d00
ChangHyeon Kim <hazasue@gmail.com>
2023-11-09 13:27:05 +0900

    :fire: Do not inherit MonoBehaviour

--- Assets/Scripts/Json/JsonManager.cs
+++ Assets/Scripts/Json/JsonManager.cs
@@ -6,22 +6,14 @@ using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class JsonManager : MonoBehaviour
+public class JsonManager
 {
-    private static JsonManager instance;
-
     public static string DEFAULT_CHARACTER_DATA_NAME = "CharacterData";
     public static string DEFAULT_CURRENT_CHARACTER_DATA_NAME = "CurrentCharacterData";
-    
-    public static JsonManager GetInstance()
-    {
-        if (instance != null) return instance;
-        instance = FindObjectOfType<JsonManager>();
-        if (instance == null) Debug.Log("There's no active JsonManager object");
-        return instance;
-    }
+    public static string DEFAULT_ENEMY_DATA_NAME = "DataTable_Enemy";
+    public static string DEFAULT_WEAPON_DATA_NAME = "DataTable_Weapon";
 
-    public void CreateJsonFile(string fileName, object obj)
+    public static void CreateJsonFile(string fileName, object obj)
     {
         // 데이터 폴더가 없다면 생성하기
         if (!File.Exists(Application.dataPath + "/Data/"))
@@ -36,7 +28,7 @@ public class JsonManager : MonoBehaviour
         fileStream.Close();
     }
 
-    public T LoadJsonFile<T>(string fileName)
+    public static T LoadJsonFile<T>(string fileName)
     {
         if (!File.Exists(Application.dataPath + "/Data/" + fileName + ".json"))
         {
