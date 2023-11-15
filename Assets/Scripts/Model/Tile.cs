e76fb94d02ed3f9bcb07c34fb5804583145be8ad
ChangHyeon Kim <hazasue@gmail.com>
2023-11-15 19:55:59 +0900

    :tada: Init Tile Script

---
+++ Assets/Scripts/Model/Tile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider player)
+    {
+        if (!player.CompareTag("player")) return;
+
+        TileManager.GetInstance().MoveTiles(this);
+    }
+}
