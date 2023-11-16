2907eefbe68a0482b9210dccffd346a221b5d60d
ChangHyeon Kim <hazasue@gmail.com>
2023-11-16 15:18:44 +0900

    :hammer: WeaponManager: weapon can be upgraded

--- Assets/Scripts/Controller/WeaponManager.cs
+++ Assets/Scripts/Controller/WeaponManager.cs
@@ -7,10 +7,13 @@ public class WeaponManager : MonoBehaviour
     private static WeaponManager instance;
     
     private static int DEFAULT_OPTION_COUNT = 4;
+    private static int MAX_UPGRADE_COUNT = 5;
 
     private Dictionary<string, WeaponInfo> weaponInfos;
+    private Dictionary<string, WeaponUpgradeInfo> weaponUpgradeInfos;
 
     private List<WeaponInfo> augmentOptions;
+    private string basicWeaponCode;
     
     // Start is called before the first frame update
     void Awake()
@@ -21,15 +24,29 @@ public class WeaponManager : MonoBehaviour
     private void init()
     {
         weaponInfos = new Dictionary<string, WeaponInfo>();
+        weaponUpgradeInfos = new Dictionary<string, WeaponUpgradeInfo>();
         augmentOptions = new List<WeaponInfo>();
         
         Dictionary<string, WeaponInfo> tempWeaponInfos =
             JsonManager.LoadJsonFile<Dictionary<string, WeaponInfo>>(JsonManager.DEFAULT_WEAPON_DATA_NAME);
+        Dictionary<string, WeaponUpgradeInfo> tempWeaponUpgradeInfo =
+            JsonManager.LoadJsonFile<Dictionary<string, WeaponUpgradeInfo>>(
+                JsonManager.DEFAULT_WEAPON_UPGRADE_DATA_NAME);
 
         foreach (KeyValuePair<string, WeaponInfo> data in tempWeaponInfos)
         {
             weaponInfos.Add(data.Key, data.Value);
         }
+
+        foreach (KeyValuePair<string, WeaponUpgradeInfo> data in tempWeaponUpgradeInfo)
+        {
+            weaponUpgradeInfos.Add(data.Key, data.Value);
+        }
+
+        basicWeaponCode =
+            JsonManager.LoadJsonFile<Dictionary<string, CharacterData>>(JsonManager.DEFAULT_CHARACTER_DATA_NAME)
+                [JsonManager.LoadJsonFile<CurrentCharacterInfo>(JsonManager.DEFAULT_CURRENT_CHARACTER_DATA_NAME).currentSelectedCode]
+                .basicWeapon;
     }
     
     public static WeaponManager GetInstance()
@@ -57,14 +74,29 @@ public class WeaponManager : MonoBehaviour
         List<string> optionCodes = new List<string>();
         augmentOptions.Clear();
 
+        // 기본 직업 무기
+        optionCodes.Add(basicWeaponCode);
+        
+        // 공용 무기
         foreach (WeaponInfo data in weaponInfos.Values)
         {
+            if (data.GetOccupation() != "common") continue;
             optionCodes.Add(data.GetCode());
         }
 
+        // 업그레이드 횟수 초과한 무기 리스트에서 제거
+        foreach (Weapon weapon in GameManager.GetInstance().GetPlayer().GetInventory().GetWeapons().Values)
+        {
+            if (weapon.GetUpgradeCount() >= MAX_UPGRADE_COUNT) optionCodes.Remove(weapon.GetCode());
+        }
+
+        int idx;
+        
         for (int i = 0; i < DEFAULT_OPTION_COUNT; i++)
         {
-            augmentOptions.Add(weaponInfos[optionCodes[Random.Range(0, optionCodes.Count)]]);
+            idx = Random.Range(0, optionCodes.Count);
+            augmentOptions.Add(weaponInfos[optionCodes[idx]]);
+            optionCodes.RemoveAt(idx);
         }
 
         return augmentOptions;
@@ -72,6 +104,22 @@ public class WeaponManager : MonoBehaviour
 
     public void ReflectAugment(int index)
     {
-        GameManager.GetInstance().GetPlayer().GetInventory().AddWeapon(augmentOptions[index]);
+        Weapon selectedWeapon;
+        if (!GameManager.GetInstance().GetPlayer().GetInventory().GetWeapons().TryGetValue(augmentOptions[index].GetCode(), out selectedWeapon))
+            selectedWeapon = null;
+        
+        if(!selectedWeapon)
+            GameManager.GetInstance().GetPlayer().GetInventory().AddWeapon(augmentOptions[index]);
+        else
+        {
+            selectedWeapon.UpgradeWeapon(weaponUpgradeInfos[augmentOptions[index].GetCode()]);
+        }
+    }
+
+    // test code
+    public void UpgradeBasicWeaponFull()
+    {
+        GameManager.GetInstance().GetPlayer().GetInventory().GetWeapons()[basicWeaponCode]
+            .UpgradeWeapon(weaponUpgradeInfos[basicWeaponCode]);
     }
 }
