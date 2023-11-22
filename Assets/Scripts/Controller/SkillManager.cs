fe4dbd4ac4e98c05cdc6712ea3440b81be6d1da1
ChangHyeon Kim <hazasue@gmail.com>
2023-11-23 00:48:48 +0900

    :hammer: Update SkillManager

--- Assets/Scripts/Controller/SkillManager.cs
+++ Assets/Scripts/Controller/SkillManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
+    private static SkillManager instance;
+    
+    private Dictionary<string, SkillInfo> skillInfos;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        init();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void init()
     {
-        
+        skillInfos = JsonManager.LoadJsonFile<Dictionary<string, SkillInfo>>(JsonManager.DEFAULT_SKILL_DATA_NAME);
+        instance = this;
+    }
+
+    public static SkillManager GetInstance()
+    {
+        if (instance != null) return instance;
+        instance = FindObjectOfType<SkillManager>();
+        if (instance == null) Debug.Log("There's no active SkillManager object");
+        return instance;
+    }
+
+    public SkillInfo GetSkillInfo(string code)
+    {
+        if (skillInfos.ContainsKey(code)) return skillInfos[code];
+        else
+        {
+            Debug.Log("Invalid skill code: " + code);
+            return null;
+        }
     }
 }
