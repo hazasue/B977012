bdc7a8ee520a7a52700e613ee23f02be39dab2db
ChangHyeon Kim <hazasue@gmail.com>
2023-11-22 22:25:01 +0900

    :hammer: Add variables

--- Assets/Scripts/Model/Equipment.cs
+++ Assets/Scripts/Model/Equipment.cs
@@ -12,14 +12,35 @@ public class Equipment : MonoBehaviour
 
     private EquipmentType type;
     private string code;
+    private string purpose;
+    private string buyStatus;
+    private int price;
 
-    public void Init(string code, EquipmentType type = EquipmentType.WEAPON)
+    // Buy
+    public void Init(string code, string purpose, string buyStatus, int price, EquipmentType type = EquipmentType.WEAPON)
     {
         this.code = code;
+        this.purpose = purpose;
+        this.buyStatus = buyStatus;
+        this.price = price;
+        this.type = type;
+    }
+    
+    // Equip
+    public void Init(string code, string purpose, EquipmentType type = EquipmentType.WEAPON)
+    {
+        this.code = code;
+        this.purpose = purpose;
         this.type = type;
     }
 
     public string GetCode() { return code; }
+
+    public string GetPurpose() { return purpose; }
+
+    public string GetBuyStatus() { return buyStatus; }
+
+    public int GetPrice() { return price; }
 
     public EquipmentType GetType() { return type; }
 }
