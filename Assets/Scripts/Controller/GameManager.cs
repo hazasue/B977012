b3d18997771f96161350079db43929e700339eb2
ChangHyeon Kim <hazasue@gmail.com>
2023-11-22 22:26:57 +0900

    :hammer: Add rewards when game cleared

--- Assets/Scripts/Controller/GameManager.cs
+++ Assets/Scripts/Controller/GameManager.cs
@@ -13,12 +13,17 @@ public class GameManager : MonoBehaviour
     }
 
     private static GameManager instance;
-
-    private static string DEFAULT_CHARACTER_NAME = "warrior";
+    
+    private static string DEFAULT_WEAPON_CODE_WARRIOR = "warriorMelee";
+    private static string DEFAULT_WEAPON_CODE_WIZARD = "wizardTracking";
     
     // attributes
     private GameStatus gameStatus;
     private float time;
+
+    private Dictionary<string, CharacterData> characterDatas;
+    private string characterIndex;
+    private StageInfo stageInfo;
     
     // associations
     private Player player;
@@ -38,9 +43,11 @@ public class GameManager : MonoBehaviour
 
         Time.timeScale = 1;
 
-        Dictionary<string, CharacterData> characterDatas = JsonManager.LoadJsonFile<Dictionary<string, CharacterData>>(JsonManager.DEFAULT_CHARACTER_DATA_NAME);
-        
-        string characterIndex = JsonManager.LoadJsonFile<CurrentCharacterInfo>(JsonManager.DEFAULT_CURRENT_CHARACTER_DATA_NAME).currentSelectedCode;
+        characterDatas = JsonManager.LoadJsonFile<Dictionary<string, CharacterData>>(JsonManager.DEFAULT_CHARACTER_DATA_NAME);
+        characterIndex = JsonManager.LoadJsonFile<CurrentCharacterInfo>(JsonManager.DEFAULT_CURRENT_CHARACTER_DATA_NAME).currentSelectedCode;
+        stageInfo =
+            JsonManager.LoadJsonFile<Dictionary<string, StageInfo>>(JsonManager.DEFAULT_STAGE_DATA_NAME)[
+                characterDatas[characterIndex].currentStage.ToString()];
         
         player = Instantiate(Resources.Load<Player>("prefabs/characters/" + characterDatas[characterIndex].playerType));
         player.Init(characterDatas[characterIndex].maxHp,
@@ -73,6 +80,35 @@ public class GameManager : MonoBehaviour
         gameStatus = GameStatus.CLEAR;
         PauseGame();
         mUIManager.ActivateClearScreen();
+
+    }
+
+    public void AddReward()
+    {
+        characterDatas[characterIndex].coin += stageInfo.basicReward;
+        if (!characterDatas[characterIndex].clearStages[characterDatas[characterIndex].currentStage])
+        {
+            characterDatas[characterIndex].clearStages[characterDatas[characterIndex].currentStage] = true;
+            string weaponCode = "";
+            switch (characterDatas[characterIndex].playerType)
+            {
+                case "WARRIOR":
+                    weaponCode = DEFAULT_WEAPON_CODE_WARRIOR + (characterDatas[characterIndex].currentStage + 2) + "1";
+                    break;
+                
+                case "WIZARD":
+                    weaponCode = DEFAULT_WEAPON_CODE_WIZARD + (characterDatas[characterIndex].currentStage + 2) + "1";
+                    break;
+                
+                default:
+                    Debug.Log("Invalid player type: " + characterDatas[characterIndex].playerType);
+                    break;
+            }
+
+            characterDatas[characterIndex].equipmentCodes.Add(weaponCode);
+            characterDatas[characterIndex].order++;
+        }
+        JsonManager.CreateJsonFile(JsonManager.DEFAULT_CHARACTER_DATA_NAME, characterDatas);
     }
 
     public Player GetPlayer() { return player; }
