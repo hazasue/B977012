77434a212d78c34b4edb438879cf8aa9ff3b5a3d
ChangHyeon Kim <hazasue@gmail.com>
2023-11-22 22:23:42 +0900

    :hammer: Change what to parse

--- Assets/Scripts/CSVConverter.cs
+++ Assets/Scripts/CSVConverter.cs
@@ -11,6 +11,8 @@ public class CSVConverter : MonoBehaviour
     private static string CSV_FILENAME_ENEMY = "DataTable_Enemy";
     private static string CSV_FILENAME_STAGE = "DataTable_Stage";
     private static string CSV_FILENAME_CHARACTER = "DataTable_Character";
+
+    private static int DEFAULT_ORDER_NUMBER = 1;
     
     public void ConvertCsvToJson()
     {
@@ -29,7 +31,9 @@ public class CSVConverter : MonoBehaviour
                     (int)weaponInfo["Projectile"],
                     float.Parse(weaponInfo["Range"].ToString()),
                     float.Parse(weaponInfo["Speed"].ToString()),
-                    weaponInfo["Occupation"].ToString()
+                    weaponInfo["Occupation"].ToString(),
+                    (int)weaponInfo["Order"],
+                    (int)weaponInfo["Price"]
                 ));
         }
 
@@ -50,6 +54,7 @@ public class CSVConverter : MonoBehaviour
         }
         JsonManager.CreateJsonFile(JsonManager.DEFAULT_WEAPON_UPGRADE_DATA_NAME, upgradeInfos);
         
+        // enemy
         List<Dictionary<string, object>> EnemyDB = CSVReader.Read(CSV_FILENAME_ENEMY);
         Dictionary<string, EnemyInfo> enemyInfos = new Dictionary<string, EnemyInfo>();
         foreach (Dictionary<string, object> enemyInfo in EnemyDB)
@@ -67,8 +72,8 @@ public class CSVConverter : MonoBehaviour
                 ));
         }
         JsonManager.CreateJsonFile(JsonManager.DEFAULT_ENEMY_DATA_NAME, enemyInfos);
-
-
+        
+        // stage
         List<Dictionary<string, object>> StageDB = CSVReader.Read(CSV_FILENAME_STAGE);
         Dictionary<string, StageInfo> stageInfos = new Dictionary<string, StageInfo>();
         List<string> normalEnemies = new List<string>();
@@ -83,23 +88,26 @@ public class CSVConverter : MonoBehaviour
             bossEnemies.Add(stageInfo["BossEnemy1"].ToString());
             bossEnemies.Add(stageInfo["BossEnemy2"].ToString());
             bossEnemies.Add(stageInfo["BossEnemy3"].ToString());
-            
+
             stageInfos.Add(stageInfo["StageCode"].ToString(),
                 new StageInfo(stageInfo["StageCode"].ToString(),
                     (int)stageInfo["NormalEnemyCount"],
                     (int)stageInfo["BossEnemyCount"],
                     normalEnemies.ToList(),
-                    bossEnemies.ToList()
+                    bossEnemies.ToList(),
+                    (int)stageInfo["BasicReward"]
                 ));
         }
         JsonManager.CreateJsonFile(JsonManager.DEFAULT_STAGE_DATA_NAME, stageInfos);
         
-        
+        // character info(basic)
         List<Dictionary<string, object>> CharacterDB = CSVReader.Read(CSV_FILENAME_CHARACTER);
         Dictionary<string, CharacterData> characterInfos = new Dictionary<string, CharacterData>();
         foreach (Dictionary<string, object> characterInfo in CharacterDB)
         {
             List<string> equipmentCodes = new List<string>();
+            List<bool> clearStages = new List<bool>();
+            for (int i = 0; i < 3; i++) { clearStages.Add(false); }
             equipmentCodes.Add(characterInfo["BasicWeapon"].ToString());
             characterInfos.Add(characterInfo["CharacterCode"].ToString(),
                 new CharacterData(
@@ -109,7 +117,9 @@ public class CSVConverter : MonoBehaviour
                     (int)characterInfo["Damage"],
                     float.Parse(characterInfo["Speed"].ToString()),
                     (int)characterInfo["Armor"],
-                    equipmentCodes)
+                    DEFAULT_ORDER_NUMBER,
+                    equipmentCodes,
+                    clearStages)
             );
         }
         
