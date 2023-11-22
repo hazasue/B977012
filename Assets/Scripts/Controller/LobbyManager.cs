aeebc058de5aa5a9396229ca05942ea25a9217ff
ChangHyeon Kim <hazasue@gmail.com>
2023-11-22 22:26:37 +0900

    :hammer: Update Shop functions

--- Assets/Scripts/Controller/LobbyManager.cs
+++ Assets/Scripts/Controller/LobbyManager.cs
@@ -10,6 +10,13 @@ public class LobbyManager : MonoBehaviour
     private const string DEFAULT_SHOP_NAME = "shop";
     private const string DEFAULT_INVENTORY_NAME = "inventory";
     private const string DEFAULT_UPGRADE_NAME = "upgrade";
+    private const string DEFAULT_PURPOSE_BUY = "buy";
+    private const string DEFAULT_PURPOSE_EQUIP = "equip";
+    private const string DEFAULT_BUY_STATUS_LOW_ORDER = "lowOrder";
+    private const string DEFAULT_BUY_STATUS_LOW_MONEY = "lowMoney";
+    private const string DEFAULT_BUY_STATUS_ALREADY_HAVE = "alreadyHave";
+    private const string DEFAULT_BUY_STATUS_BUYABLE = "buyable";
+    
     private static Color INACTIVE_COLOR = new Color32(128, 128, 128, 255);
     private static Color ACTIVE_COLOR = new Color32(255, 255, 255, 255);
     
@@ -18,12 +25,19 @@ public class LobbyManager : MonoBehaviour
     private string characterCode;
     private Equipment currentEquipment;
 
+    public GameObject equipButton;
+    public GameObject buyButton;
+
     public Image selectedImage;
+    public TMP_Text selectedEquipStatus;
     public TMP_Text selectedCode;
     public TMP_Text selectedType;
     public TMP_Text selectedInfo;
+    public TMP_Text selectedOccupation;
+    public TMP_Text selectedEquipPrice;
 
     public Image equipButtonSprite;
+    public Image buyButtonSprite;
     public Transform viewport;
 
     public Equipment equipment;
@@ -47,12 +61,17 @@ public class LobbyManager : MonoBehaviour
     {
         // shop contents: all job waepons - gained weapons
         // inventory contents: CharacterData.equipmentCodes
+        equipButton.SetActive(false);
+        buyButton.SetActive(false);
+        
         switch (name)
         {
             case DEFAULT_SHOP_NAME:
+                buyButton.SetActive(true);
                 break;
             
             case DEFAULT_INVENTORY_NAME:
+                equipButton.SetActive(true);
                 break;
             
             case DEFAULT_UPGRADE_NAME:
@@ -69,34 +88,89 @@ public class LobbyManager : MonoBehaviour
     private void initContents(string name)
     {
         // Clear contents
+        currentEquipment = null;
         for (int i = viewport.childCount - 1; i >= 0; i--)
         {
             Destroy(viewport.GetChild(i).gameObject);
         }
 
-        foreach (string code in characterDatas[characterCode].equipmentCodes)
+        switch (name)
         {
-            Equipment tempEquipment = Instantiate(equipment, viewport, true);
-            tempEquipment.Init(code);
-            tempEquipment.GetComponent<Button>().onClick.AddListener(ClickEquipment);
+            case DEFAULT_SHOP_NAME:
+                List<WeaponInfo> ownWeapons = new List<WeaponInfo>();
+                string tempCode;
+                foreach (WeaponInfo info in WeaponManager.GetInstance().GetEquiptableWeapons())
+                {
+                    tempCode = info.GetCode();
+                    if (getBuyStatus(tempCode) != DEFAULT_BUY_STATUS_BUYABLE)
+                    {
+                        ownWeapons.Add(info);
+                        continue;
+                    }
+                    
+                    Equipment tempEquipment = Instantiate(equipment, viewport, true);
+                    tempEquipment.Init(tempCode, DEFAULT_PURPOSE_BUY, DEFAULT_BUY_STATUS_BUYABLE, info.GetPrice());
+                    tempEquipment.GetComponent<Button>().onClick.AddListener(ClickEquipment);
 
-            if (code == characterDatas[characterCode].basicWeapon)
-            {
-                currentEquipment = tempEquipment;
-                updateSelectedEquipData();
-                updateEquipButton();
-            }
+                    if (!currentEquipment)
+                    {
+                        currentEquipment = tempEquipment;
+                        updateSelectedEquipData(currentEquipment.GetPurpose());
+                        updateButton(currentEquipment.GetPurpose());
+                    }
+                }
+
+                foreach (WeaponInfo info in ownWeapons)
+                {
+                    tempCode = info.GetCode();
+                    Equipment tempEquipment = Instantiate(equipment, viewport, true);
+                    tempEquipment.Init(tempCode, DEFAULT_PURPOSE_BUY, getBuyStatus(tempCode), info.GetPrice());
+                    tempEquipment.GetComponent<Button>().onClick.AddListener(ClickEquipment);
+                    tempEquipment.GetComponent<RawImage>().color = INACTIVE_COLOR;
+                    
+                    if (!currentEquipment)
+                    {
+                        currentEquipment = tempEquipment;
+                        updateSelectedEquipData(currentEquipment.GetPurpose());
+                        updateButton(currentEquipment.GetPurpose());
+                    }
+                }
+                break;
+            
+            case DEFAULT_INVENTORY_NAME:
+                foreach (string code in characterDatas[characterCode].equipmentCodes)
+                {
+                    Equipment tempEquipment = Instantiate(equipment, viewport, true);
+                    tempEquipment.Init(code, DEFAULT_PURPOSE_EQUIP);
+                    tempEquipment.GetComponent<Button>().onClick.AddListener(ClickEquipment);
+
+                    if (code == characterDatas[characterCode].basicWeapon)
+                    {
+                        currentEquipment = tempEquipment;
+                        updateSelectedEquipData(currentEquipment.GetPurpose());
+                        updateButton(currentEquipment.GetPurpose());
+                    }
+                }
+                break;
+            
+            case DEFAULT_UPGRADE_NAME:
+                break;
+            
+            default:
+                Debug.Log("Invalid screen name: " + name);
+                break;
         }
+        
     }
 
     public void ClickEquipment()
     {
         currentEquipment = EventSystem.current.currentSelectedGameObject.transform.GetComponent<Equipment>();
-        updateSelectedEquipData();
-        updateEquipButton();
+        updateSelectedEquipData(currentEquipment.GetPurpose());
+        updateButton(currentEquipment.GetPurpose());
     }
 
-    private void updateSelectedEquipData()
+    private void updateSelectedEquipData(string purpose)
     {
         // show infos
         switch (currentEquipment.GetType())
@@ -111,10 +185,28 @@ public class LobbyManager : MonoBehaviour
                 selectedType.text = weaponInfo.GetType();
                 selectedInfo.text = weaponInfo.GetDamage() + " / " + weaponInfo.GetDuration() + " / " +
                                     weaponInfo.GetDelay() + " / " + weaponInfo.GetProjectile();
+                selectedOccupation.text = weaponInfo.GetOccupation() + " " + weaponInfo.GetOrder();
                 break;
 
             default:
                 Debug.Log("Invalid equipment type: " + currentEquipment.GetType());
+                break;
+        }
+        
+        switch (purpose)
+        {
+            case DEFAULT_PURPOSE_BUY:
+                selectedEquipStatus.text = currentEquipment.GetBuyStatus();
+                selectedEquipPrice.text = "Price: " + currentEquipment.GetPrice().ToString();
+                break;
+            
+            case DEFAULT_PURPOSE_EQUIP:
+                selectedEquipStatus.text = "";
+                selectedEquipPrice.text = "";
+                break;
+            
+            default:
+                Debug.Log("Invalid purpose: " + purpose);
                 break;
         }
     }
@@ -122,6 +214,7 @@ public class LobbyManager : MonoBehaviour
     public void EquipEquipment()
     {
         if (!currentEquipment) return;
+        if (characterDatas[characterCode].basicWeapon == currentEquipment.GetCode()) return;
         
         switch (currentEquipment.GetType())
         {
@@ -137,18 +230,64 @@ public class LobbyManager : MonoBehaviour
                 break;
         }
 
-        updateEquipButton();
+        updateButton(currentEquipment.GetPurpose());
 
         JsonManager.CreateJsonFile(JsonManager.DEFAULT_CHARACTER_DATA_NAME, characterDatas);
     }
 
-    private void updateEquipButton()
+    public void BuyEquipment()
     {
-        if (currentEquipment.GetCode() == characterDatas[characterCode].basicWeapon)
-            equipButtonSprite.color = INACTIVE_COLOR;
-        else
+        if (!currentEquipment) return;
+        if (characterDatas[characterCode].coin < currentEquipment.GetPrice()) return;
+        foreach (string equipment in characterDatas[characterCode].equipmentCodes)
         {
-            equipButtonSprite.color = ACTIVE_COLOR;
+            if (equipment == currentEquipment.GetCode()) return;
         }
+
+        characterDatas[characterCode].coin -=
+            WeaponManager.GetInstance().GetWeaponInfo(currentEquipment.GetCode()).GetPrice();
+        characterDatas[characterCode].equipmentCodes.Add(currentEquipment.GetCode());
+
+        JsonManager.CreateJsonFile(JsonManager.DEFAULT_CHARACTER_DATA_NAME, characterDatas);
+        
+        initContents(DEFAULT_SHOP_NAME);
+    }
+
+    private void updateButton(string purpose)
+    {
+        switch (purpose)
+        {
+            case DEFAULT_PURPOSE_BUY:
+                if (currentEquipment.GetBuyStatus() == DEFAULT_BUY_STATUS_BUYABLE)
+                    buyButtonSprite.color = ACTIVE_COLOR;
+                else
+                {
+                    buyButtonSprite.color = INACTIVE_COLOR;
+                }
+                break;
+
+            case DEFAULT_PURPOSE_EQUIP:
+                if (currentEquipment.GetCode() == characterDatas[characterCode].basicWeapon)
+                    equipButtonSprite.color = INACTIVE_COLOR;
+                else
+                {
+                    equipButtonSprite.color = ACTIVE_COLOR;
+                }
+                break;
+        }
+    }
+
+    private string getBuyStatus(string code)
+    {
+        WeaponInfo tempWeapon = WeaponManager.GetInstance().GetWeaponInfo(code);
+        if (tempWeapon.GetOrder() > characterDatas[characterCode].order) return DEFAULT_BUY_STATUS_LOW_ORDER;
+        if (tempWeapon.GetPrice() > characterDatas[characterCode].coin) return DEFAULT_BUY_STATUS_LOW_MONEY;
+        foreach (string data in characterDatas[characterCode].equipmentCodes)
+        {
+            if (tempWeapon.GetCode() == data) return DEFAULT_BUY_STATUS_ALREADY_HAVE;
+        }
+
+        return DEFAULT_BUY_STATUS_BUYABLE;
+
     }
 }
