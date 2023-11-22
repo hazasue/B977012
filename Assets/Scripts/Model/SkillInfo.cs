4d7c8a4c98683642f7d13b02c4d97c635f363062
ChangHyeon Kim <hazasue@gmail.com>
2023-11-22 23:14:46 +0900

    :tada: Init SkillInfo

---
+++ Assets/Scripts/Model/SkillInfo.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SkillInfo
+{
+    public string skillType;
+    public float delay;
+    public float duration;
+    public string stat;
+    public float value;
+    public int damage;
+    public int projectile;
+
+    public SkillInfo(string skillType, float delay, float duration, string stat, float value, int damage,
+        int projectile)
+    {
+        this.skillType = skillType;
+        this.delay = delay;
+        this.duration = duration;
+        this.stat = stat;
+        this.value = value;
+        this.damage = damage;
+        this.projectile = projectile;
+    }
+}
