edce46bf1ff484f7cb578714f897aae3be0bd1a0
ChangHyeon Kim <hazasue@gmail.com>
2023-11-30 11:12:23 +0900

    :hammer: Update StageManager: Change Stage image's alpha value

--- Assets/Scripts/Controller/StageManager.cs
+++ Assets/Scripts/Controller/StageManager.cs
@@ -7,12 +7,18 @@ using UnityEngine.EventSystems;
 public class StageManager : MonoBehaviour
 {
     private const float DEFAULT_ONE_BLOCK_WIDTH = 700f;
-    
+    private static Color32 TRANSLUCENT_COLOR = new Color32(255, 255, 255, 104);
+    private static Color32 OPACITY_COLOR = new Color32(255, 255, 255, 255);
+
+    public GameObject stagePanel;
     public Transform stageContent;
+    public Stage stageObject;
 
     private Dictionary<string, CharacterData> characterDatas;
     private Dictionary<string, StageInfo> stageInfos;
     private string characterIndex;
+
+    private int currentStage;
 
     void Start()
     {
@@ -21,6 +27,8 @@ public class StageManager : MonoBehaviour
 
     void Update()
     {
+        if (!stagePanel.activeSelf) return;
+        
         if (Input.GetMouseButtonUp(0))
         {
             adjustStageContentPosition();
@@ -29,33 +37,54 @@ public class StageManager : MonoBehaviour
 
     private void init()
     {
-        characterDatas = JsonManager.LoadJsonFile<Dictionary<string, CharacterData>>(JsonManager.DEFAULT_CHARACTER_DATA_NAME);
+        LoadCharacterDatas();
         stageInfos = JsonManager.LoadJsonFile<Dictionary<string, StageInfo>>(JsonManager.DEFAULT_STAGE_DATA_NAME);
-        characterIndex =
-            JsonManager.LoadJsonFile<CurrentCharacterInfo>(JsonManager.DEFAULT_CURRENT_CHARACTER_DATA_NAME)
-                .currentSelectedCode;
+
+
+        Stage tempStage;
+        foreach (StageInfo data in stageInfos.Values)
+        {
+            tempStage = Instantiate(stageObject, stageContent, true);
+            tempStage.Init(data.stageCode);
+        }
 
         stageContent.localPosition =
             new Vector3(-DEFAULT_ONE_BLOCK_WIDTH * characterDatas[characterIndex].currentStage, 0f, 0f);
+
+        currentStage = -1;
+        ChangeStageAlphaValue();
+    }
+
+    public void LoadCharacterDatas()
+    {
+        characterDatas = JsonManager.LoadJsonFile<Dictionary<string, CharacterData>>(JsonManager.DEFAULT_CHARACTER_DATA_NAME);
+        characterIndex =
+            JsonManager.LoadJsonFile<CurrentCharacterInfo>(JsonManager.DEFAULT_CURRENT_CHARACTER_DATA_NAME)
+                .currentSelectedCode;
     }
 
     private void adjustStageContentPosition()
     {
-        StartCoroutine(moveProperPosition(findProperPosition()));
+        StartCoroutine(moveProperPosition(convertIndexToVector3(findProperStageIndex())));
     }
 
-    private Vector3 findProperPosition()
+    private Vector3 convertIndexToVector3(int index)
+    {
+        return new Vector3(-DEFAULT_ONE_BLOCK_WIDTH * index, 0f, 0f);
+    }
+
+    private int findProperStageIndex()
     {
         for (int i = 0; i < stageInfos.Count; i++)
         {
             if (stageContent.localPosition.x >= -DEFAULT_ONE_BLOCK_WIDTH * i - DEFAULT_ONE_BLOCK_WIDTH / 2
                 && stageContent.localPosition.x <= -DEFAULT_ONE_BLOCK_WIDTH * i + DEFAULT_ONE_BLOCK_WIDTH / 2)
             {
-                return new Vector3(-DEFAULT_ONE_BLOCK_WIDTH * i, 0f, 0f);
+                return i;
             }
         }
 
-        return Vector3.zero;
+        return -1;
     }
 
     private IEnumerator moveProperPosition(Vector3 properPosition)
@@ -67,12 +96,27 @@ public class StageManager : MonoBehaviour
         if (stageContent.localPosition.x <= properPosition.x + 5f
             && stageContent.localPosition.x >= properPosition.x - 5f)
         {
+            int index = (int)-properPosition.x / (int)DEFAULT_ONE_BLOCK_WIDTH;
             stageContent.localPosition = properPosition;
-            characterDatas[characterIndex].currentStage = (int)-properPosition.x / 500;
+            characterDatas[characterIndex].currentStage = index;
             JsonManager.CreateJsonFile(JsonManager.DEFAULT_CHARACTER_DATA_NAME, characterDatas);
             yield break;
         }
         
         StartCoroutine(moveProperPosition(properPosition));
+    }
+
+    public void ChangeStageAlphaValue()
+    {
+        if (currentStage == findProperStageIndex()) return;
+        
+        currentStage = findProperStageIndex();
+        
+        for (int i = 0; i < stageContent.childCount; i++)
+        {
+            stageContent.GetChild(i).GetComponent<Image>().color = TRANSLUCENT_COLOR;
+        }
+
+        stageContent.GetChild(currentStage).GetComponent<Image>().color = OPACITY_COLOR;
     }
 }
