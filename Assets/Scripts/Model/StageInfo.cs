b3d909efbaa41eeeb8d02c2e227a891333b705ee
ChangHyeon Kim <hazasue@gmail.com>
2024-04-01 11:18:53 +0900

    :hammer: Add stage init infos

--- Assets/Scripts/Model/StageInfo.cs
+++ Assets/Scripts/Model/StageInfo.cs
@@ -7,20 +7,33 @@ public class StageInfo
 {
     public string stageCode;
     public int normalEnemyCount;
+    public int specialEnemyCount;
+    public int eliteEnemyCount;
     public int bossEnemyCount;
+    public int guardPatternCount;
     public List<string> normalEnemies;
+    public List<string> specialEnemies;
+    public List<string> eliteEnemies;
     public List<string> bossEnemies;
+    public List<string> guardPatterns;
     public int basicReward;
     public string texture;
 
-    public StageInfo(string stageCode, int normalEnemyCount, int bossEnemyCount, List<string> normalEnemies,
-        List<string> bossEnemies, int basicReward, string texture)
+    public StageInfo(string stageCode, int normalEnemyCount, int specialEnemyCount, int eliteEnemyCount,
+        int bossEnemyCount, int guardPatternCount, List<string> normalEnemies, List<string> specialEnemies, List<string> eliteEnemies,
+        List<string> bossEnemies, List<string> guardPatterns, int basicReward, string texture)
     {
         this.stageCode = stageCode;
         this.normalEnemyCount = normalEnemyCount;
+        this.specialEnemyCount = specialEnemyCount;
+        this.eliteEnemyCount = eliteEnemyCount;
         this.bossEnemyCount = bossEnemyCount;
+        this.guardPatternCount = guardPatternCount;
         this.normalEnemies = normalEnemies;
+        this.specialEnemies = specialEnemies;
+        this.eliteEnemies = eliteEnemies;
         this.bossEnemies = bossEnemies;
+        this.guardPatterns = guardPatterns;
         this.basicReward = basicReward;
         this.texture = texture;
     }
