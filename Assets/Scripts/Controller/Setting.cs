dfe5b8b02ba723078203dd3f7e7effd23dc2a973
ChangHyeon Kim <hazasue@gmail.com>
2024-04-01 11:16:27 +0900

    :hammer: Can toggle screen resolution

--- Assets/Scripts/Controller/Setting.cs
+++ Assets/Scripts/Controller/Setting.cs
@@ -4,13 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Setting : MonoBehaviour
 {
     private static Setting instance;
-    
-    private const int DEFAULT_SCREEN_RESOLUTION_X = 1920;
-    private const int DEFAULT_SCREEN_RESOLUTION_Y = 1080;
+
+    private static int[] DEFAULT_SCREEN_RESOLUTION_SMALL = new int[2]{ 1280, 720 };
+    private static int[] DEFAULT_SCREEN_RESOLUTION_NORMAL = new int[2]{ 1920, 1080 };
+    private static int[] DEFAULT_SCREEN_RESOLUTION_BIG = new int[2]{ 2560, 1440 };
+    private const int DEFAULT_SCREEN_RESOLUTION_GAP_X = 640;
+    private const int DEFAULT_SCREEN_RESOLUTION_GAP_Y = 360;
     private const bool DEFAULT_FULL_SCREEN_STATE = true;
     private const bool DEFAULT_DAMAGE_SHOW_STATE = true;
     private const float DEFAULT_SOUND_VOLUME = 0.2f;
@@ -20,11 +24,14 @@ public class Setting : MonoBehaviour
     private const string DEFAULT_NAME_SFX = "SFX";
     private const string DEFAULT_NAME_FULL_SCREEN = "FULLSCREEN";
     private const string DEFAULT_NAME_SHOW_DAMAGE = "SHOWDAMAGE";
-
+    
+    public TMP_Text resolutionText;
     public Toggle fullScreenToggle;
     public Slider bgmSlider;
     public Slider sfxSlider;
     public Toggle showDamageToggle;
+
+    public float resolutionMagnification;
 
     private SettingInfo settingInfo;
 
@@ -40,11 +47,13 @@ public class Setting : MonoBehaviour
         else
         {
             SettingInfo tempInfo =
-                new SettingInfo(new int[] { DEFAULT_SCREEN_RESOLUTION_X, DEFAULT_SCREEN_RESOLUTION_Y },
-                    DEFAULT_FULL_SCREEN_STATE, DEFAULT_SOUND_VOLUME, DEFAULT_SOUND_VOLUME, DEFAULT_DAMAGE_SHOW_STATE);
+                new SettingInfo(DEFAULT_SCREEN_RESOLUTION_NORMAL, DEFAULT_FULL_SCREEN_STATE, DEFAULT_SOUND_VOLUME,
+                    DEFAULT_SOUND_VOLUME, DEFAULT_DAMAGE_SHOW_STATE);
             JsonManager.CreateJsonFile(JsonManager.DEFAULT_SETTING_DATA_NAME, tempInfo);
             settingInfo = tempInfo;
         }
+
+        resolutionMagnification = 0f;
         
         fullScreenToggle.isOn = settingInfo.fullScreen;
         bgmSlider.value = settingInfo.volumeBgm;
@@ -67,11 +76,46 @@ public class Setting : MonoBehaviour
         return instance;
     }
 
+    public void ToggleMagnificationPositive(bool positive)
+    {
+        if (positive) 
+            resolutionMagnification = 1f;
+        else
+        {
+            resolutionMagnification = -1f;
+        }
+    }
+    
     public void ChangeSetting(string target)
     {
         switch (target)
         {
             case DEFAULT_NAME_RESOLUTION:
+                if (resolutionMagnification == 0f) return;
+                
+                if (resolutionMagnification > 0f)
+                {
+                    if(settingInfo.screenResolution[0] >= DEFAULT_SCREEN_RESOLUTION_BIG[0])
+                        settingInfo.screenResolution = (int[])DEFAULT_SCREEN_RESOLUTION_SMALL.Clone();
+                    else
+                    {
+                        settingInfo.screenResolution[0] += DEFAULT_SCREEN_RESOLUTION_GAP_X;
+                        settingInfo.screenResolution[1] += DEFAULT_SCREEN_RESOLUTION_GAP_Y;
+                    }
+                }
+                else
+                {
+                    if(settingInfo.screenResolution[0] <= DEFAULT_SCREEN_RESOLUTION_SMALL[0])
+                        settingInfo.screenResolution = (int[])DEFAULT_SCREEN_RESOLUTION_BIG.Clone();
+                    else
+                    {
+                        settingInfo.screenResolution[0] -= DEFAULT_SCREEN_RESOLUTION_GAP_X;
+                        settingInfo.screenResolution[1] -= DEFAULT_SCREEN_RESOLUTION_GAP_Y;
+                    }
+                }
+
+                Debug.Log(DEFAULT_SCREEN_RESOLUTION_SMALL[0]);
+                resolutionText.text = $"{settingInfo.screenResolution[0]} * {settingInfo.screenResolution[1]}";
                 break;
             
             case DEFAULT_NAME_FULL_SCREEN:
@@ -102,6 +146,7 @@ public class Setting : MonoBehaviour
             case DEFAULT_NAME_RESOLUTION:
             case DEFAULT_NAME_FULL_SCREEN:
                 Screen.SetResolution(settingInfo.screenResolution[0], settingInfo.screenResolution[1], settingInfo.fullScreen);
+                resolutionText.text = $"{settingInfo.screenResolution[0]} * {settingInfo.screenResolution[1]}";
                 break;
             
             case DEFAULT_NAME_BGM:
