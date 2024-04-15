d892a93075ced1f3a39bbf864a9d0874fe1630fb
ChangHyeon Kim <hazasue@gmail.com>
2024-04-15 12:32:48 +0900

    :hammer: Add attack type to enemy projectile

--- Assets/Scripts/Model/Enemy/EnemyProjectile.cs
+++ Assets/Scripts/Model/Enemy/EnemyProjectile.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour
 {
+    public enum AttackType
+    {
+        ONE_OFF,
+        CONTINUING
+    }
+
+    private const float DEFAULT_DAMAGE_DELAY = 0.25f;
+    
     private int damage;
     private float speed;
     private Vector3 attackDirection;
-    
+    private AttackType attackType;
+
+    private Player player;
+    private float time;
+
     // Update is called once per frame
     void Update()
     {
         move();
+        if (player != null
+            && attackType == AttackType.CONTINUING)
+            damagePlayer();
     }
     
-    public void Init(int damage, float speed, Vector3 attackDirection, float duration)
+    public void Init(int damage, float speed, Vector3 attackDirection, float duration, AttackType attackType)
     {
+        player = null;
+        time = 0f;
         this.damage = damage;
         this.speed = speed;
         this.attackDirection = attackDirection;
+        this.attackType = attackType;
         Destroy(this.gameObject, duration);
     }
     
@@ -31,7 +49,27 @@ public class EnemyProjectile : MonoBehaviour
     {
         if (!obj.CompareTag("player")) return;
 
-        Player player = obj.GetComponent<Player>();
+        player = obj.GetComponent<Player>();
         player.TakeDamage(damage);
+    }
+
+    public void OnTriggerExit(Collider obj)
+    {
+        if (!obj.CompareTag("player")) return;
+
+        time = 0f;
+        player = null;
+    }
+
+    private void damagePlayer()
+    {
+        time += Time.deltaTime;
+
+        Debug.Log(time);
+        if (time >= DEFAULT_DAMAGE_DELAY)
+        {
+            time -= DEFAULT_DAMAGE_DELAY;
+            player.TakeDamage(damage);
+        }
     }
 }
