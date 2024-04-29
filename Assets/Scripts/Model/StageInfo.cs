e8034c2ec1cc431680aee4526889776b3a39dbc3
ChangHyeon Kim <hazasue@gmail.com>
2024-04-29 12:07:02 +0900

    :hammer: Stage Info have ranged enemy infos

--- Assets/Scripts/Model/StageInfo.cs
+++ Assets/Scripts/Model/StageInfo.cs
@@ -7,11 +7,13 @@ public class StageInfo
 {
     public string stageCode;
     public int normalEnemyCount;
+    public int rangedEnemyCount;
     public int specialEnemyCount;
     public int eliteEnemyCount;
     public int bossEnemyCount;
     public int guardPatternCount;
     public List<string> normalEnemies;
+    public List<string> rangedEnemies;
     public List<string> specialEnemies;
     public List<string> eliteEnemies;
     public List<string> bossEnemies;
@@ -19,17 +21,19 @@ public class StageInfo
     public int basicReward;
     public string texture;
 
-    public StageInfo(string stageCode, int normalEnemyCount, int specialEnemyCount, int eliteEnemyCount,
-        int bossEnemyCount, int guardPatternCount, List<string> normalEnemies, List<string> specialEnemies, List<string> eliteEnemies,
+    public StageInfo(string stageCode, int normalEnemyCount, int rangedEnemyCount, int specialEnemyCount, int eliteEnemyCount,
+        int bossEnemyCount, int guardPatternCount, List<string> normalEnemies, List<string> rangedEnemies, List<string> specialEnemies, List<string> eliteEnemies,
         List<string> bossEnemies, List<string> guardPatterns, int basicReward, string texture)
     {
         this.stageCode = stageCode;
         this.normalEnemyCount = normalEnemyCount;
+        this.rangedEnemyCount = rangedEnemyCount;
         this.specialEnemyCount = specialEnemyCount;
         this.eliteEnemyCount = eliteEnemyCount;
         this.bossEnemyCount = bossEnemyCount;
         this.guardPatternCount = guardPatternCount;
         this.normalEnemies = normalEnemies;
+        this.rangedEnemies = rangedEnemies;
         this.specialEnemies = specialEnemies;
         this.eliteEnemies = eliteEnemies;
         this.bossEnemies = bossEnemies;
