2eeab09dac270f7ac642feed10f582d56c3b174b
ChangHyeon Kim <hazasue@gmail.com>
2024-05-13 00:36:59 +0900

    :hammer: Enemy Projectile can attack enemy

--- Assets/Scripts/Model/Enemy/EnemyProjectile.cs
+++ Assets/Scripts/Model/Enemy/EnemyProjectile.cs
@@ -7,7 +7,8 @@ public class EnemyProjectile : MonoBehaviour
     public enum AttackType
     {
         ONE_OFF,
-        CONTINUING
+        CONTINUING,
+        EXPLOSION
     }
 
     private const float DEFAULT_DAMAGE_DELAY = 0.25f;
@@ -18,20 +19,21 @@ public class EnemyProjectile : MonoBehaviour
     private AttackType attackType;
 
     private Player player;
+    private List<Enemy> enemies;
     private float time;
 
     // Update is called once per frame
     void Update()
     {
         move();
-        if (player != null
-            && attackType == AttackType.CONTINUING)
-            damagePlayer();
+        if (attackType == AttackType.CONTINUING)
+            damageCharacters();
     }
     
     public void Init(int damage, float speed, Vector3 attackDirection, float duration, AttackType attackType)
     {
         player = null;
+        enemies = new List<Enemy>();
         time = 0f;
         this.damage = damage;
         this.speed = speed;
@@ -47,28 +49,65 @@ public class EnemyProjectile : MonoBehaviour
     
     public void OnTriggerEnter(Collider obj)
     {
-        if (!obj.CompareTag("player")) return;
-
-        player = obj.GetComponent<Player>();
-        player.TakeDamage(damage);
+        switch (obj.tag)
+        {
+            case "player":
+                player = obj.GetComponent<Player>();
+                player.TakeDamage(damage);
+                break;
+            
+            case "enemy":
+                if (attackType == AttackType.ONE_OFF) return;
+                Enemy enemy = obj.GetComponent<Enemy>();
+                enemies.Add(enemy);
+                enemy.TakeDamage(damage);
+                break;
+            
+            default:
+                break;
+            
+        }
     }
 
     public void OnTriggerExit(Collider obj)
     {
-        if (!obj.CompareTag("player")) return;
-
-        time = 0f;
-        player = null;
+        switch (obj.tag)
+        {
+            case "player":
+                time = 0f;
+                player = null;
+                break;
+            
+            case "enemy":
+                if (attackType == AttackType.ONE_OFF) return;
+                Enemy enemy = obj.GetComponent<Enemy>();
+                enemies.Remove(enemy);
+                break;
+            
+            default:
+                break;
+            
+        }
     }
 
-    private void damagePlayer()
+    private void damageCharacters()
     {
         time += Time.deltaTime;
         
         if (time >= DEFAULT_DAMAGE_DELAY)
         {
             time -= DEFAULT_DAMAGE_DELAY;
-            player.TakeDamage(damage);
+            for (int i = enemies.Count - 1; i >= 0; i--)
+            {
+                if (!enemies[i].gameObject.activeSelf) enemies.RemoveAt(i);
+            }
+            if (player != null) player.TakeDamage(damage);
+
+            foreach (Enemy enemy in enemies)
+            {
+                enemy.TakeDamage(damage);
+            }
+            
         }
     }
 }
