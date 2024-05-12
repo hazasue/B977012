e4ef62aab73d2247a795fb69c48ac27883a8d6f7
ChangHyeon Kim <hazasue@gmail.com>
2024-05-13 00:42:49 +0900

    :hammer: Stage Info saves separated enemy lists

--- Assets/Scripts/Model/StageInfo.cs
+++ Assets/Scripts/Model/StageInfo.cs
@@ -8,36 +8,42 @@ public class StageInfo
     public string stageCode;
     public int normalEnemyCount;
     public int rangedEnemyCount;
-    public int specialEnemyCount;
+    public int groupEnemyCount;
+    public int guardEnemyCount;
+    public int explosiveEnemyCount;
     public int eliteEnemyCount;
     public int bossEnemyCount;
-    public int guardPatternCount;
     public List<string> normalEnemies;
     public List<string> rangedEnemies;
-    public List<string> specialEnemies;
+    public List<string> groupEnemies;
+    public List<string> guardEnemies;
+    public List<string> explosiveEnemies;
     public List<string> eliteEnemies;
     public List<string> bossEnemies;
-    public List<string> guardPatterns;
     public int basicReward;
     public string texture;
 
-    public StageInfo(string stageCode, int normalEnemyCount, int rangedEnemyCount, int specialEnemyCount, int eliteEnemyCount,
-        int bossEnemyCount, int guardPatternCount, List<string> normalEnemies, List<string> rangedEnemies, List<string> specialEnemies, List<string> eliteEnemies,
-        List<string> bossEnemies, List<string> guardPatterns, int basicReward, string texture)
+    public StageInfo(string stageCode, int normalEnemyCount, int rangedEnemyCount, int groupEnemyCount,
+        int guardEnemyCount, int explosiveEnemyCount, int eliteEnemyCount,
+        int bossEnemyCount, List<string> normalEnemies, List<string> rangedEnemies, List<string> groupEnemies,
+        List<string> guardEnemies, List<string> explosiveEnemies, List<string> eliteEnemies,
+        List<string> bossEnemies, int basicReward, string texture)
     {
         this.stageCode = stageCode;
         this.normalEnemyCount = normalEnemyCount;
         this.rangedEnemyCount = rangedEnemyCount;
-        this.specialEnemyCount = specialEnemyCount;
+        this.groupEnemyCount = groupEnemyCount;
+        this.guardEnemyCount = guardEnemyCount;
+        this.explosiveEnemyCount = explosiveEnemyCount;
         this.eliteEnemyCount = eliteEnemyCount;
         this.bossEnemyCount = bossEnemyCount;
-        this.guardPatternCount = guardPatternCount;
         this.normalEnemies = normalEnemies;
         this.rangedEnemies = rangedEnemies;
-        this.specialEnemies = specialEnemies;
+        this.groupEnemies = groupEnemies;
+        this.guardEnemies = guardEnemies;
+        this.explosiveEnemies = explosiveEnemies;
         this.eliteEnemies = eliteEnemies;
         this.bossEnemies = bossEnemies;
-        this.guardPatterns = guardPatterns;
         this.basicReward = basicReward;
         this.texture = texture;
     }
