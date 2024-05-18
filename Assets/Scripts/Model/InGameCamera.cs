d1a4b9d890dcddd383f3c5a7e3aa250e0dc717ab
ChangHyeon Kim <hazasue@gmail.com>
2024-05-18 21:50:10 +0900

    :hammer: Update camera boss spawn settings

--- Assets/Scripts/Model/InGameCamera.cs
+++ Assets/Scripts/Model/InGameCamera.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class InGameCamera : MonoBehaviour
 {
+    private static Vector3 DEFAULT_PLAYER_CAMERA_POSITION = new Vector3(0f, 20f, 0f);
+    private static Vector3 DEFAULT_PLAYER_CAMERA_ROTATION = new Vector3(90f, 0f, 0f);
+    private static Vector3 DEFAULT_BOSS_CAMERA_POSITION = new Vector3(0f, 15f, -15f);
+    private static Vector3 DEFAULT_BOSS_CAMERA_ROTATION = new Vector3(45f, 0f, 0f);
+    
     private Transform player;
+    private Transform target;
+
+    private Vector3 cameraPosition;
 
     void Start()
     {
         player = GameManager.GetInstance().GetPlayer().transform;
+        target = player;
+        cameraPosition = DEFAULT_PLAYER_CAMERA_POSITION;
+        this.transform.rotation = Quaternion.Euler(DEFAULT_PLAYER_CAMERA_ROTATION);
     }
 
     // Update is called once per frame
@@ -19,6 +30,21 @@ public class InGameCamera : MonoBehaviour
 
     private void MoveCamera()
     {
-        this.transform.position = player.position + new Vector3(0f, 20f, 0f);
+        this.transform.position = target.position + cameraPosition;
+    }
+
+    public IEnumerator ChaseBossEnemy(Transform bossEnemy, float duration)
+    {
+        if (duration <= 0f) yield break;
+        target = bossEnemy;
+        cameraPosition = DEFAULT_BOSS_CAMERA_POSITION;
+        this.transform.rotation = Quaternion.Euler(DEFAULT_BOSS_CAMERA_ROTATION);
+
+        yield return new WaitForSeconds(duration);
+
+        target = player;
+        cameraPosition = DEFAULT_PLAYER_CAMERA_POSITION;
+        this.transform.rotation = Quaternion.Euler(DEFAULT_PLAYER_CAMERA_ROTATION);
+
     }
 }
